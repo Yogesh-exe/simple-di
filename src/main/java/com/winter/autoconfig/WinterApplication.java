8dd8beb0ac1732ea1720a17ea5c8c4d525d2d0ed
Yogeshwar Bagul <mr.yogeshwarbagul@gmail.com>
2021-04-16 11:32:55 +0530

    Choosing the right factory

--- src/main/java/com/winter/autoconfig/WinterApplication.java
+++ src/main/java/com/winter/autoconfig/WinterApplication.java
@@ -1,17 +1,25 @@
 package com.winter.autoconfig;
 
 import com.winter.factory.BeanFactory;
+import com.winter.factory.annotation.EnableAutoConfiguration;
 
 public class WinterApplication {
 
-	private static FactoryCreator factoryCreator = new BruteFactoryCreator();
+	private static FactoryCreator factoryCreator;
 
 	public static BeanFactory run(Class<?> mainClass) {
 		return run(mainClass, new String[0]);
 	}
 
 	public static BeanFactory run(Class<?> mainClass, String... args) {
-		return factoryCreator.createFactory(mainClass, args);
+		if(mainClass.getAnnotation(EnableAutoConfiguration.class) != null) {
+			factoryCreator = new AnnotaionBasedFactoryCreator();
+			return factoryCreator.createFactory(mainClass, args);
+		}
+		else {
+			factoryCreator = new BruteFactoryCreator();
+			return factoryCreator.createFactory(mainClass, args);
+		}
 	}
 
 }
